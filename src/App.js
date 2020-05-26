7b28a3ed7622775d8a8cbfd5bcaadd6e89b3e91e
Singgih <gokonggokong71@gmail.com>
2020-05-26 12:39:39 +0700

    setup project dan library

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Welcomme App Js</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
