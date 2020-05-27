1b9377b592c0248ac776c59c0339f52551fc67a5
Singgih <gokonggokong71@gmail.com>
2020-05-27 10:23:21 +0700

    add some navigation and some pages (register, login)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {GetStarted} from './pages';
-
+import {NavigationContainer} from '@react-navigation/native';
+import Router from './router';
 const App = () => {
-  return <GetStarted />;
+  return (
+    <NavigationContainer>
+      <Router />
+    </NavigationContainer>
+  );
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
